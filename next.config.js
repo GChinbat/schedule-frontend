b17a49eb83df1bd79a1b3a4f4ca245ffabcbac4e
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-01 05:10:40 +0800

    build: Add antd dependency

--- next.config.js
+++ next.config.js
@@ -1,7 +1,29 @@
 const path = require('path');
+const withCss = require('@zeit/next-css');
 
-module.exports = {
-  webpack(config) {
+module.exports = withCss({
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style\/css.*?/;
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
+        },
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ];
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
+      });
+    }
+
     config.resolve.alias['@'] = path.join(__dirname, 'src');
     config.module.rules.push({
       test: /\.svg$/,
@@ -12,4 +34,4 @@ module.exports = {
     });
     return config;
   },
-};
+});
