7ed73798cbd24b300ade9b439f462c6bc536afaa
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-01 04:20:13 +0800

    feat: Add typedefs and slug prop

--- src/api/schedule.ts
+++ src/api/schedule.ts
@@ -1,9 +1,27 @@
 import gql from 'graphql-tag';
 
+export type Time = {
+  hours: number;
+  minutes: number;
+};
+export type ScheduleEntry = {
+  lessonGroup: {
+    slug: string;
+    groupName: string;
+    lesson: {
+      name: string;
+      teachers: string[];
+    };
+  };
+  startTime: Time;
+  endTime: Time;
+};
+
 export const GET_SCHEDULE = gql`
   query {
     schedule {
       lessonGroup {
+        slug
         groupName
         lesson {
           name
