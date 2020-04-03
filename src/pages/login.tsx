74b17d6778526ebcccbb36cc8583225854241702
Bayalagmaa <bayalagmaanest@gmail.com>
2020-04-03 18:50:21 +0800

    fix: Login page layout

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,22 +1,31 @@
 import React from 'react';
-import { Tabs } from 'antd';
+import styled from 'styled-components';
+import { Tabs, Layout } from 'antd';
 
 import Login from '@/components/Login';
 import Register from '@/components/Register';
 
 const { TabPane } = Tabs;
 
+const Content = styled(Layout.Content)`
+  padding: 0 50px;
+  margin-top: 10px;
+`;
+
 function LoginPage() {
   return (
-    <Tabs defaultActiveKey="login" tabPosition="top">
-      <TabPane key="login" tab="Нэвтрэх">
-        <Login />
-      </TabPane>
-      <TabPane key="register" tab="Бүртгүүлэх">
-        <Register />
-      </TabPane>
-    </Tabs>
+    <Content>
+      <Tabs defaultActiveKey="login" tabPosition="top">
+        <TabPane key="login" tab="Нэвтрэх">
+          <Login />
+        </TabPane>
+        <TabPane key="register" tab="Бүртгүүлэх">
+          <Register />
+        </TabPane>
+      </Tabs>
+    </Content>
   );
 }
 
 export default LoginPage;
+  
