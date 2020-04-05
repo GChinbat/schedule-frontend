4a9b86a8f9861350e0a02c79d480e0a0564d5f1b
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-05 19:48:13 +0800

    feat(experiment): Add service worker

--- next.config.js
+++ next.config.js
@@ -1,37 +1,40 @@
 const path = require('path');
 const withCss = require('@zeit/next-css');
+const withOffline = require('next-offline');
 
-module.exports = withCss({
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      const antStyles = /antd\/.*?\/style\/css.*?/;
-      const origExternals = [...config.externals];
-      config.externals = [
-        (context, request, callback) => {
-          if (request.match(antStyles)) return callback();
-          if (typeof origExternals[0] === 'function') {
-            origExternals[0](context, request, callback);
-          } else {
-            callback();
-          }
+module.exports = withOffline(
+  withCss({
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        const antStyles = /antd\/.*?\/style\/css.*?/;
+        const origExternals = [...config.externals];
+        config.externals = [
+          (context, request, callback) => {
+            if (request.match(antStyles)) return callback();
+            if (typeof origExternals[0] === 'function') {
+              origExternals[0](context, request, callback);
+            } else {
+              callback();
+            }
+          },
+          ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+        ];
+
+        config.module.rules.unshift({
+          test: antStyles,
+          use: 'null-loader',
+        });
+      }
+
+      config.resolve.alias['@'] = path.join(__dirname, 'src');
+      config.module.rules.push({
+        test: /\.svg$/,
+        issuer: {
+          test: /\.(js|ts)x?$/,
         },
-        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-      ];
-
-      config.module.rules.unshift({
-        test: antStyles,
-        use: 'null-loader',
+        use: ['@svgr/webpack'],
       });
-    }
-
-    config.resolve.alias['@'] = path.join(__dirname, 'src');
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
-      },
-      use: ['@svgr/webpack'],
-    });
-    return config;
-  },
-});
+      return config;
+    },
+  }),
+);
