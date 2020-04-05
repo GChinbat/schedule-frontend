4c236da7dd02e9963c7589d6c74028dc06c54ad4
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-05 20:31:00 +0800

    feat(experiment): Add runtime caching for routes

--- next.config.js
+++ next.config.js
@@ -4,37 +4,59 @@ const withOffline = require('next-offline');
 const withPlugins = require('next-compose-plugins');
 const withManifest = require('next-manifest');
 
-module.exports = withPlugins([withCss, withOffline], {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      const antStyles = /antd\/.*?\/style\/css.*?/;
-      const origExternals = [...config.externals];
-      config.externals = [
-        (context, request, callback) => {
-          if (request.match(antStyles)) return callback();
-          if (typeof origExternals[0] === 'function') {
-            origExternals[0](context, request, callback);
-          } else {
-            callback();
-          }
-        },
-        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
-      ];
-
-      config.module.rules.unshift({
-        test: antStyles,
-        use: 'null-loader',
-      });
-    }
-
-    config.resolve.alias['@'] = path.join(__dirname, 'src');
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts)x?$/,
+module.exports = withPlugins(
+  [
+    withCss,
+    [
+      withOffline,
+      {
+        runtimeCaching: [
+          {
+            urlPattern: /^https?.*/,
+            handler: 'NetworkFirst',
+            options: {
+              cacheName: 'offlineCache',
+              expiration: {
+                maxEntries: 200,
+              },
+            },
+          },
+        ],
       },
-      use: ['@svgr/webpack'],
-    });
-    return config;
+    ],
+  ],
+  {
+    webpack: (config, { isServer }) => {
+      if (isServer) {
+        const antStyles = /antd\/.*?\/style\/css.*?/;
+        const origExternals = [...config.externals];
+        config.externals = [
+          (context, request, callback) => {
+            if (request.match(antStyles)) return callback();
+            if (typeof origExternals[0] === 'function') {
+              origExternals[0](context, request, callback);
+            } else {
+              callback();
+            }
+          },
+          ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+        ];
+
+        config.module.rules.unshift({
+          test: antStyles,
+          use: 'null-loader',
+        });
+      }
+
+      config.resolve.alias['@'] = path.join(__dirname, 'src');
+      config.module.rules.push({
+        test: /\.svg$/,
+        issuer: {
+          test: /\.(js|ts)x?$/,
+        },
+        use: ['@svgr/webpack'],
+      });
+      return config;
+    },
   },
-});
+);
