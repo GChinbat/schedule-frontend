656e9f0ba7d7aba1c778c8634ab4970cc5b75f53
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-05 15:38:44 +0800

    feat: Remove login section

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,31 +1,67 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
-import { Tabs, Layout } from 'antd';
+import { useRouter } from 'next/router';
+import { useLazyQuery } from '@apollo/react-hooks';
+import { Form, Input, Button, Layout } from 'antd';
 
-import Login from '@/widgets/Login';
-import Register from '@/widgets/Register';
+import { LOGIN } from '@/api/auth';
+
 import { withApollo } from '@/api/withApollo';
-
-const { TabPane } = Tabs;
 
 const Content = styled(Layout.Content)`
   padding: 0 50px;
-  margin-top: 10px;
+  margin: 30px;
 `;
 
+const layout = {
+  labelCol: { span: 6 },
+  wrapperCol: { span: 12 },
+};
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+};
+
 function LoginPage() {
+  const router = useRouter();
+  const [login, { data, loading }] = useLazyQuery(LOGIN);
+
+  const onFinish = (formData) => {
+    login({ variables: formData });
+  };
+  useEffect(() => {
+    if (data?.login) {
+      localStorage.setItem('token', data.login);
+      router.push('/');
+    }
+  }, [data]);
+
   return (
     <Content>
-      <Tabs defaultActiveKey="login" tabPosition="top">
-        <TabPane key="login" tab="Нэвтрэх">
-          <Login />
-        </TabPane>
-        <TabPane key="register" tab="Бүртгүүлэх">
-          <Register />
-        </TabPane>
-      </Tabs>
+      <Form {...layout} name="login" onFinish={onFinish}>
+        <Form.Item
+          label="Нэвтрэх нэр"
+          name="username"
+          rules={[{ required: true, message: 'Please input your username!' }]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Нууц үг"
+          name="password"
+          rules={[{ required: true, message: 'Please input your password!' }]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button loading={loading} type="primary" htmlType="submit">
+            Нэвтрэх
+          </Button>
+        </Form.Item>
+      </Form>
     </Content>
   );
 }
 
-export default withApollo({ ssr: false })(LoginPage);
+export default withApollo({ ssr: true })(LoginPage);
