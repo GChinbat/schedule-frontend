19b0655003884ebbe59b19515c0a63be70745c05
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-05 19:00:24 +0800

    fix: Remove admin actions in schedule

--- src/pages/schedule.tsx
+++ src/pages/schedule.tsx
@@ -1,18 +1,13 @@
 import Head from 'next/head';
-import React, { useState } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 
 import { useQuery } from '@apollo/react-hooks';
 import { withApollo } from '@/api/withApollo';
 import { GET_SCHEDULE } from '@/api/schedule';
 
-import { Layout, Button } from 'antd';
+import { Layout } from 'antd';
 import Schedule from '@/widgets/Schedule';
-import AddScheduleItemModal from '@/modals/AddScheduleItem';
-import EditScheduleItemModal from '@/modals/EditScheduleItem';
-
-import { useLoginCheck } from '@/hooks/useLoginCheck';
-import { EditScheduleStateProvider } from '@/hooks/EditScheduleState';
 
 const Content = styled(Layout.Content)`
   padding: 0 50px;
@@ -20,28 +15,17 @@ const Content = styled(Layout.Content)`
 `;
 
 function Home() {
-  const { data, loading, refetch } = useQuery(GET_SCHEDULE);
-  const [addingSchedule, setAddingSchedule] = useState(false);
-  useLoginCheck();
+  const { data, loading } = useQuery(GET_SCHEDULE);
 
   return (
-    <EditScheduleStateProvider>
+    <>
       <Head>
         <title>Schedule</title>
       </Head>
       <Content>
-        <Button type="primary" onClick={() => setAddingSchedule(true)}>
-          Хуваарийн хичээл нэмэх
-        </Button>
         <Schedule loading={loading} schedule={data?.schedule} />
       </Content>
-      <AddScheduleItemModal
-        show={addingSchedule}
-        refetch={refetch}
-        setModalState={setAddingSchedule}
-      />
-      <EditScheduleItemModal refetch={refetch} />
-    </EditScheduleStateProvider>
+    </>
   );
 }
 
