075b71c5d03b224832e773ec6b2d35e384bd5fd6
Zolboobayar Gantumur <zolboobayar@nhs.edu.mn>
2020-04-07 10:39:22 +0800

    fix: Check for token expiry before authorization

--- src/hooks/useLoginCheck.tsx
+++ src/hooks/useLoginCheck.tsx
@@ -1,10 +1,17 @@
+import jwtDecode from 'jwt-decode';
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 export function useLoginCheck() {
   const router = useRouter();
   useEffect(() => {
-    if (!localStorage.getItem('token')) {
+    try {
+      const { exp } = jwtDecode(localStorage.getItem('token'));
+      if (exp < Date.now() / 1000) {
+        throw Error('JWT token has expired');
+      }
+    } catch {
+      localStorage.removeItem('token');
       router.push('/login');
     }
   }, [router]);
